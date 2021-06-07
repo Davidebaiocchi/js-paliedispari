7b02d0facda5d8ead8b1552bb06a5193aeabfa49
davide <davide.baiocchi97@gmail.com>
2021-06-07 16:24:56 +0200

    end end

--- js/script.js
+++ js/script.js
@@ -1,33 +1,33 @@
-// // Pari e Dispari: L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri. 
-// // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto
+// Pari e Dispari: L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri. 
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto
 
-// // l'utente sceglie pari o dispari:
-// var pariDispari = prompt('Pari o Dispari');
-// console.log(pariDispari);
-// var numberUser = parseInt(prompt('Scegli un numero da 1 a 5'));
-// console.log(numberUser);
+// l'utente sceglie pari o dispari:
+var pariDispari = prompt('Pari o Dispari');
+console.log(pariDispari);
+var numberUser = parseInt(prompt('Scegli un numero da 1 a 5'));
+console.log(numberUser);
 
-// // Generiamo un numero random sempre da 1 a 5:
-// function numberRandom() {
-//     var num = Math.round(Math.random() * 4) +1;
-//     return num;
-// }
-// var number = numberRandom();
-// console.log(number);
+// Generiamo un numero random sempre da 1 a 5:
+function numberRandom() {
+    var num = Math.round(Math.random() * 4) +1;
+    return num;
+}
+var number = numberRandom();
+console.log(number);
 
-// // Facciamo la somma dei due numeri:
-// var somma = numberUser + number;
-// console.log(somma);
+// Facciamo la somma dei due numeri:
+var somma = numberUser + number;
+console.log(somma);
 
-// // Scopriamo se la somma è pari o dispari:
-// var sommaPariDispari = pariODispari();
+// Scopriamo se la somma è pari o dispari:
+var sommaPariDispari = pariODispari();
 
-// function pariODispari() {
+function pariODispari() {
 
-//     if (somma % 2 == 0) {
-//         return 'Pari'
-//     } else {
-//         return 'Dispari'
-//     }
-// }  
-// console.log(pariOdispari);
+    if (somma % 2 == 0) {
+        return 'Pari'
+    } else {
+        return 'Dispari'
+    }
+}  
+console.log(pariOdispari);
