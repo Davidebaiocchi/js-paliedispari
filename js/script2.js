4612183a8db7ce9c0f9e780e9476b3e947f3d98e
davide <davide.baiocchi97@gmail.com>
2021-06-07 16:23:53 +0200

    end

--- js/script2.js
+++ js/script2.js
@@ -1,1 +1,21 @@
-// Palidroma: Chiedere all’utente di inserire una parola. Creare una funzione per capire se la parola inserita è palindroma
+// Palidroma: Chiedere all’utente di inserire una parola. 
+// Creare una funzione per capire se la parola inserita è palindroma
+
+// chiedere parola
+var parola = prompt('inserisci una parola');
+console.log(parola);
+
+// girare la parola e r confrontarla con quella iniziale
+var parolaInversa = invertiParola(parola);
+
+if(parola == parolaInversa){
+    console.log('la parola è palindroma');
+  } else {
+    console.log('la parola non è palindroma');
+  }
+
+function invertiParola(str){
+  var strInversa = str.split('').reverse().join('');  
+  return strInversa;
+}
+
